129f4ad079085722fc5626eb020508b5be153295
beuleu <billyklotz789@gmail.com>
2023-01-09 23:19:14 +0100

    header, A propos, home, mobile menu DONE

---
+++ mobileMenu.js
@@ -0,0 +1,6 @@
+const toggleMenu = document.querySelector(".menu-button")
+const navLinks = document.querySelector(".navlinks")
+toggleMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("navlinks-mobile")
+    navLinks.classList.toggle("textshadow")
+})
